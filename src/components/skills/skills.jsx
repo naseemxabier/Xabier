42127d5d119ea8caedd015fe46d72e57122d223d
Xabier Naseem Mohammad <naseemxabier@gmail.com>
2023-04-09 11:19:51 +0200

    falta server

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -13,38 +13,41 @@ function Skills() {
         <>
             <section id="skills">
                 <h1 id="tech">TECH SKILLS</h1>
-                <div class="skill">
-                    <img src={javascript} alt="JavaScript" />
-                    <p>JavaScript (ES6)</p>
+                <div className="container">
+                    <div className="skill">
+                        <img src={javascript} alt="JavaScript" />
+                        <p>JavaScript (ES6)</p>
+                    </div>
+                    <div className="skill">
+                        <img src={python} alt="Python" />
+                        <p>Python</p>
+                    </div>
+                    <div className="skill">
+                        <img src={html} alt="HTML5" />
+                        <p>HTML5</p>
+                    </div>
+                    <div className="skill">
+                        <img src={css} alt="CSS3" />
+                        <p>CSS3</p>
+                    </div>
+                    <div className="skill">
+                        <img src={react} alt="React" />
+                        <p>React</p>
+                    </div>
+                    <div className="skill">
+                        <img src={node} alt="NodeJS" />
+                        <p>NodeJS</p>
+                    </div>
+                    <div className="skill">
+                        <img src={django} alt="Django" />
+                        <p>Django</p>
+                    </div>
+                    <div className="skill">
+                        <img src={mongo} alt="MongoDB" />
+                        <p>MongoDB</p>
+                    </div>
                 </div>
-                <div class="skill">
-                    <img src={python} alt="Python" />
-                    <p>Python</p>
-                </div>
-                <div class="skill">
-                    <img src={html} alt="HTML5" />
-                    <p>HTML5</p>
-                </div>
-                <div class="skill">
-                    <img src={css} alt="CSS3" />
-                    <p>CSS3</p>
-                </div>
-                <div class="skill">
-                    <img src={react} alt="React" />
-                    <p>React</p>
-                </div>
-                <div class="skill">
-                    <img src={node} alt="NodeJS" />
-                    <p>NodeJS</p>
-                </div>
-                <div class="skill">
-                    <img src={django} alt="Django" />
-                    <p>Django</p>
-                </div>
-                <div class="skill">
-                    <img src={mongo} alt="MongoDB" />
-                    <p>MongoDB</p>
-                </div>
+
             </section>
 
         </>
